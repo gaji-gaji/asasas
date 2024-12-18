da3343bf7551f816f958df4f35d52126de378dce
gaji-gaji <83190912+gaji-gaji@users.noreply.github.com>
2024-12-19 01:20:07 +0900

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypts');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = express.Router();
